587d16d8f35bc55ca267cf5a936fc6e235d4dde3
RUKI <mohrukiardiansyahruki@gmail.com>
2025-12-17 11:16:23 +0700

    tugas pertemuan 11

---
+++ tugas.11.py
@@ -0,0 +1,6 @@
+character = input("masukan karakter: ")
+input_jumlah = int(input("masukan jumlah;"))
+cetak = " " + character + " "
+
+for i in range (1, input_jumlah + 1):
+    print((cetak * i).center(input_jumlah * 2))
